b94a4884ba0272bcbaa975845761e1e76271453f
Havyx <everluca@outlook.com>
2021-05-26 13:03:47 -0300

    :sparkles: create queue & publish message

--- main.go
+++ main.go
@@ -18,4 +18,43 @@ func main()  {
 
 	fmt.Println("Successfully connected to rabbitMQ")
 
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue", 
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+	
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body: []byte("Hello World"),
+		},
+	)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("Successfully Publish method to queue!")
+
 }
